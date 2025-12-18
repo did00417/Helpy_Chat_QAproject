e8f20fd2ef26ee7dee9c30c82f821e624d83e3e7
[QA3]양정은 <did00417@naver.com>
2025-12-18 20:01:37 +0900

    TC-TO-005~TC-TO-007 완료

---
+++ tests/test_ppt_generation.py
@@ -0,0 +1,112 @@
+from json import tool
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from pages.login_page import LoginPage
+from pages.tool_page import ToolPage
+from utils.driver import get_driver
+from utils.helper import (
+    log_test_start,
+    log_test_failure,
+    save_screenshot
+    )
+
+'''
+===========================
+테스트 케이스:TC-TO-005~TC-TO-007 진행중
+코드 작성자: 양정은
+===========================
+'''
+
+def test_ppt_generation():
+    test_name = "AI 헬피챗 수업 지도안 클릭 테스트"
+    
+    driver = get_driver()
+    driver.get("https://qaproject.elice.io/ai-helpy-chat")
+    start_time = time.time()
+    
+    login_page = LoginPage(driver)
+    tool_page = ToolPage(driver)
+    
+    try:
+        #테스트 시작
+        log_test_start(test_name)
+        
+        # 로그인
+        login_page.login()
+        
+        # 도구 아이콘 클릭
+        tool_page.click_tool()
+        
+        # PPT 생성 클릭
+        tool_page.open_ppt_generation_card()
+        
+        # PPT 생성에 필요한 내용 입력
+        ppt_title = "인공지능 요구사항 종류"
+        ppt_instruc = "인공지능 서비스 요구사항 도출 및 명세화에서 인공지는 서비스 요구사항 도출과 인공지능 서비스 요구사항 명세화에 대해서 이미지와 도표를 추가해서 중학생이 이해 할 수 있는 수준으로 설명"
+        ppt_slide = 3
+        ppt_section =  2
+        
+        tool_page.input_ppt_content(ppt_title, ppt_instruc, ppt_slide, ppt_section)
+        
+        time.sleep(2)
+        
+        tool_page.get_generate_button()
+        tool_page.again_generate_click()
+        
+        time.sleep(2)
+        
+        assert tool_page.wait_downlord_button()   
+   
+    except Exception as e:
+        print("코드의 작동이 비정상적입니다.")
+        save_screenshot(driver, "get_ppt_generation")
+        log_test_failure(test_name, str(e), time.time() - start_time)
+        raise e
+
+    finally:
+        driver.quit()
+        
+# 필수 입력 값 미입력시 다시 생성 버튼 비활성화 검증 
+def test_regenerate_disabled_without_subject():
+    test_name = "필수 입력 값 미입력시 다시 생성 버튼 비활성화 검증"
+    
+    driver = get_driver()
+    driver.get("https://qaproject.elice.io/ai-helpy-chat")
+    start_time = time.time()
+    
+    login_page = LoginPage(driver)
+    tool_page = ToolPage(driver)
+    
+    try:
+        #테스트 시작
+        log_test_start(test_name)
+        
+        # 로그인
+        login_page.login()
+        
+        # 도구 아이콘 클릭
+        tool_page.click_tool()
+        
+        # PPT 생성 클릭
+        tool_page.open_ppt_generation_card()
+        
+        # PPT 생성에 필요한 내용 입력
+        ppt_title = ""
+        ppt_instruc = "인공지능 서비스 요구사항 도출 및 명세화에서 인공지는 서비스 요구사항 도출과 인공지능 서비스 요구사항 명세화에 대해서 이미지와 도표를 추가해서 중학생이 이해 할 수 있는 수준으로 설명"
+        ppt_slide = 5
+        ppt_section =  1
+        
+        tool_page.input_ppt_content(ppt_title, ppt_instruc, ppt_slide, ppt_section)
+        time.sleep(2)
+        
+        assert not tool_page.again_btn_assert().is_enabled()
+   
+   
+    except Exception as e:
+        print("코드의 작동이 비정상적입니다.")
+        save_screenshot(driver, "get_ppt_generation")
+        log_test_failure(test_name, str(e), time.time() - start_time)
+        raise e
+
+    finally:
+        driver.quit()
